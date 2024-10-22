c522532aa8478cb046117751975c4b64627c2c9e
yunjunwoo <noinz339@gmail.com>
2024-10-22 17:22:51 +0900

    Add files via upload

---
+++ pr2.py
@@ -0,0 +1,34 @@
+# a=5
+# # print(a)
+
+# b=2
+# # print(a//b)
+
+# # print(a%b)
+
+# if a!=b:
+#     print('안녕하세요')
+# if a==b:
+#     print('아닙니다.')
+
+
+# input - 입력받다 -> 우리가 입력해주는 것
+
+# 변수이름 = input()
+
+# a=input()
+# print(a)
+
+# 문자열이랑 숫자의 차이
+# input()함수에서 문자열같은 경우 -input()
+# 숫자같은 경우에는 int(input())
+
+a=int(input())
+print(a)
+
+print('안녕하세요 저는 사과를 %d개 샀습니다.' %a)
+
+
+# 학교끝나는 시간을 입력받고
+# 저는 오늘 몇시에 하교 했습니다.를 출력하시오
+
---
+++ pr4.py
@@ -0,0 +1,48 @@
+# # for i in range(10,61):
+# #     print(i)
+
+# for i in range(10):
+#     print(i)
+#     if i==5:
+#         break
+       
+
+# 조건문 
+# if 조건1:
+#     코드1
+# elif 조건2:
+#     코드2
+# else:
+#     코드3
+
+# 숫자에는 양수 음수 0
+# num=int(input())
+
+# if num > 0:
+#     print('양수입니다')
+# elif num <0:
+#     print('음수입니다.')
+# else:
+#     print('0입니다')
+
+# 반복문 - 반복되는 내용을 자동으로 실행시키려고
+# for i in range(숫자):
+#     코드1
+
+# for i in range(5):
+#     print('안녕하세요')
+
+
+
+for i in range(3): 
+    # => i값이 0부터 3미만일 동안 첫번째 i =0 , 두번째 i =1 , 세번째 i=2    
+    # print(i)
+
+
+
+
+
+
+
+
+
